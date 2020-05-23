0662a50b1e1c5021ba8fe222b8dcda547d3f9d68
AbdulAziz2612 <43884549+AbdulAziz2612@users.noreply.github.com>
2020-05-23 16:56:57 +0530

    Incorrectly pushed

--- database.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export const ConnectDatabase = () => {
-    mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://127.0.0.1:27017/eventmanagement', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-        .then(() => console.log('Database connected'))
-        .catch((error) => console.log(error));
-};
